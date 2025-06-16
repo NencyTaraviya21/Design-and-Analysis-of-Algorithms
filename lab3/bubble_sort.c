d9ee66329bf51aad0520b1bbae89c6f125efb8f9
NencyTaraviya21 <nencytaraviya2102@gmail.com>
2025-06-16 19:31:30 +0530

    commit

---
+++ lab3/bubble_sort.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+#define N 100
+
+int readFileOfArray(const char *filename, int arr[], int n){
+    FILE *f = open(filename,"r");
+    if(!f){
+        fprintf("File not found\n");
+    }
+
+    for(int i=0; i<n; i++){
+        fscanf(f,"%d", &arr[i]);
+    }
+
+    fclose(f);
+    return 1;
+}
+
+void bubbleSort(int arr[], int size){
+    for(int i=0; i<size-1; i++){
+        int swapped = 0;
+        for(int j=0; j<size-i-1; j++){
+            int temp = arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            swapped = 1;
+        }
+        if(swapped == 0){
+            break;
+        }
+    }
+}
+
+void printArray(int arr[]){
+    for(int i=0; i<N; i++){
+        printf(arr[i]);
+    }
+}
+
+int main(){
+    int arr[N];
+    clock_t start,end;
+    double time_taken;  
+
+    if(readFileOfArray("C:/Nency/SEM-5/LAB/DAA/searching_sorting_array/best_case_100.txt",arr,N)){
+        start=clock();
+        bubbleSort(arr,N);
+        end = clock();
+        time_taken = ((double)(end-start))/CLOCKS_PER_SEC*1000;
+    }
+    printArray(arr[N]);
+}
---
+++ searching_sorting_array/array_100000.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+
+#define N 100000
+
+void bestCase(){
+    FILE *f = fopen("best_case_100000.txt","w");
+    for(int i=0; i < N; i++){
+        fprintf(f,"%d ",i);
+    }
+    fclose(f);
+}
+
+void worstCase(){
+    FILE *f = fopen("worst_case_100000.txt","w");
+    for(int i=N-1; i>0; i--){
+        fprintf(f,"%d ",i);
+    }
+    fclose(f);
+}
+
+void avgCase(){
+    FILE *f = fopen("avg_case_100000.txt","w");
+    int arr[N];
+    for(int i=0; i<N; i++){
+        arr[i] = i+1;
+    }
+
+    srand(time(NULL));
+    for(int i=0; i<N; i++){
+        int j = rand()%(i+1);
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    for(int i=0; i<N; i++){
+         fprintf(f,"%d ",arr[i]);
+    }
+    fclose(f);
+}
+
+void main(){
+    bestCase();
+    avgCase();
+    worstCase();
+}

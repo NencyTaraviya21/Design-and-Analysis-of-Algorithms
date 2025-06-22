93002ab48c13b513d0176d93af7e4f081c4c08b1
NencyTaraviya21 <nencytaraviya2102@gmail.com>
2025-06-22 17:42:42 +0530

    commit

--- lab3/insertion_sort.c
+++ lab3/insertion_sort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include<time.h>
 
-#define N 10000
+#define N 100000
 
 int readFileOfArray(const char *filename, int arr[], int n){
     FILE *f = fopen(filename,"r");
@@ -18,26 +18,19 @@ int readFileOfArray(const char *filename, int arr[], int n){
     return 1;
 }
 
-void bubbleSort(int arr[], int size){
-    for(int i=0; i<size-1; i++){
-        int swapped = 0;
-        for(int j=0; j<size-i-1; j++){
-            int temp = arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-            swapped = 1;
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        if(swapped == 0){
-            break;
-        }
+        arr[j + 1] = key;
     }
 }
 
-// void printArray(int arr[]){
-//     for(int i=0; i<N; i++){
-//         printf(arr[i]);
-//     }
-// }
 
 int main(){
     int arr[N];
@@ -47,28 +40,28 @@ int main(){
     // Best_case...
     if(readFileOfArray("C:/Nency/SEM-5/LAB/DAA/searching_sorting_array/best_case_100000.txt",arr,N)){
         start=clock();
-        bubbleSort(arr,N);
+        insertionSort(arr,N);
         end = clock();
         time_taken = ((double)(end-start))/CLOCKS_PER_SEC;
-        printf("Time taken to Sort Array Using Bubble Sort in best Case for 100000is : %f\n",time_taken);
+        printf("Time taken to Sort Array Using Insertion Sort in best Case for 100000 is : %f\n",time_taken);
     }
 
     //worst_case...
     if(readFileOfArray("C:/Nency/SEM-5/LAB/DAA/searching_sorting_array/worst_case_100000.txt",arr,N)){
         start=clock();
-        bubbleSort(arr,N);
+       insertionSort(arr,N);
         end = clock();
         time_taken = ((double)(end-start))/CLOCKS_PER_SEC;
-        printf("Time taken to Sort Array Using Bubble Sort in Worst Case for 100000 is : %f\n",time_taken);
+        printf("Time taken to Sort Array Using Insertion Sort in Worst Case for 100000 is : %f\n",time_taken);
     }
 
     // avg case...
     if(readFileOfArray("C:/Nency/SEM-5/LAB/DAA/searching_sorting_array/avg_case_100000.txt",arr,N)){
         start=clock();
-        bubbleSort(arr,N);
+        insertionSort(arr,N);
         end = clock();
         time_taken = ((double)(end-start))/CLOCKS_PER_SEC;
-        printf("Time taken to Sort Array Using Bubble Sort in avg Case for 100000 is : %f\n",time_taken);
+        printf("Time taken to Sort Array Using Insertion Sort in avg Case for 100000 is : %f\n",time_taken);
     }
     
  
